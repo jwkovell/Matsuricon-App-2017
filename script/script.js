9b3dda65450fc47b7cda99f165f729fffade02a0
jwkovell <jwkovell@gmail.com>
2017-06-01 16:03:07 -0400

    Update script.js

--- script/script.js
+++ script/script.js
@@ -4,7 +4,6 @@ function getSchedule() {
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
      console.log(this.responseText);
-      document.getElementById("printout").innerHTML = this.responseText;
     }
   };
   xhttp.open("GET", "schedule.txt", true);
